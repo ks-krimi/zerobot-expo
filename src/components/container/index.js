da6e8032d02bed789d0551c96e17f976d8fa5c75
Ks Krimi <ny.kalash@gmail.com>
2022-10-17 16:15:43 +0200

    feat: crée un composant container, wrapper du composant scrollview

---
+++ src/components/container/index.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import Constants from 'expo-constants'
+import { StyleSheet, View, ScrollView } from 'react-native'
+
+import colors from '../../config/colors'
+
+function Container({ children, style, viewStyle }) {
+  return (
+    <ScrollView contentContainerStyle={[styles.container, style]}>
+      <View style={[styles.view, viewStyle]}>{children}</View>
+    </ScrollView>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: Constants.statusBarHeight
+  },
+  view: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: colors.light,
+    alignItems: 'center'
+  }
+})
+
+export default Container
