b50001893e09a7dc01f64c5bd012e684702a4575
Ks Krimi <ny.kalash@gmail.com>
2022-10-17 16:07:51 +0200

    conf(theme): définie le thème et les styles

---
+++ src/config/colors.js
@@ -0,0 +1,11 @@
+export default {
+  primary: '#6d4c41',
+  secondary: '#009b4e',
+  black: '#000',
+  white: '#fff',
+  medium: '#6e6969',
+  grey: '#747474',
+  light: '#9c786c',
+  dark: '#40241a',
+  danger: '#ff5252'
+}
