48a3d7ec8c173aa2f48218adc9cbc1ed21bb611f
Ks Krimi <ny.kalash@gmail.com>
2022-10-17 16:38:59 +0200

    feat: affiche le welcome screen

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, View } from 'react-native'
+
+import Welcome from './src/screens/Welcome'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Welcome />
       <StatusBar style="auto" />
     </View>
   )
@@ -12,9 +14,6 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
+    flex: 1
   }
 })
