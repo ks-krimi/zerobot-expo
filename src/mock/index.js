6049c4f7b6ba3b64ec3d89d35aceeb700fb3ced7
Ks Krimi <ny.kalash@gmail.com>
2022-10-18 09:01:47 +0200

    feat: crée des données pour mock

---
+++ src/mock/index.js
@@ -0,0 +1,109 @@
+export default [
+  {
+    id: 1,
+    content: 'Bonsoir, que puise vous aider ?',
+    by: 'zerobot'
+  },
+  {
+    id: 2,
+    content: 'Bonsoir, qui ete vous ?',
+    by: 'me'
+  },
+  {
+    id: 3,
+    content:
+      "Moi c'est Zérobot, un robot développer par Monsieur FANOMEZANTSOA Herifiandry Marc Nic",
+    by: 'zerobot'
+  },
+  {
+    id: 4,
+    content:
+      "Quel age a tu ? Es-tu célibataire? Combien d'enfants possède tu ?",
+    by: 'me'
+  },
+  {
+    id: 5,
+    content: '100 ans, Monsieur',
+    by: 'zerobot'
+  },
+  {
+    id: 6,
+    content: "T'es une fille ou un garçon ?",
+    by: 'me'
+  },
+  {
+    id: 7,
+    content: 'Je suis une fille',
+    by: 'zerobot'
+  },
+  {
+    id: 8,
+    content: 'Bonsoir, que puise vous aider ?',
+    by: 'zerobot'
+  },
+  {
+    id: 9,
+    content: 'Bonsoir, qui ete vous ?',
+    by: 'me'
+  },
+  {
+    id: 10,
+    content: "Moi c'est Zérobot",
+    by: 'zerobot'
+  },
+  {
+    id: 11,
+    content: 'Quel age a tu ?',
+    by: 'me'
+  },
+  {
+    id: 21,
+    content: '100 ans, Monsieur',
+    by: 'zerobot'
+  },
+  {
+    id: 12,
+    content: "T'es une fille ou un garçon ?",
+    by: 'me'
+  },
+  {
+    id: 13,
+    content: 'Je suis une fille',
+    by: 'zerobot'
+  },
+  {
+    id: 14,
+    content: 'Bonsoir, que puise vous aider ?',
+    by: 'zerobot'
+  },
+  {
+    id: 15,
+    content: 'Bonsoir, qui ete vous ?',
+    by: 'me'
+  },
+  {
+    id: 16,
+    content: "Moi c'est Zérobot",
+    by: 'zerobot'
+  },
+  {
+    id: 17,
+    content: 'Quel age a tu ?',
+    by: 'me'
+  },
+  {
+    id: 18,
+    content: '100 ans, Monsieur',
+    by: 'zerobot'
+  },
+  {
+    id: 19,
+    content: "T'es une fille ou un garçon ?",
+    by: 'me'
+  },
+  {
+    id: 20,
+    content: 'Je suis une fille',
+    by: 'zerobot'
+  }
+]
