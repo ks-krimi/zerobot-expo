b91fe8ba9ae3283ef5d59c5c1f0331fdec14abc4
Ks Krimi <ny.kalash@gmail.com>
2022-10-19 19:01:17 +0200

    feat: ajout du text input pour saisir un message

--- src/screens/Chat.js
+++ src/screens/Chat.js
@@ -1,6 +1,8 @@
+import { MaterialCommunityIcons } from '@expo/vector-icons'
 import React, { useRef } from 'react'
-import { FlatList, StyleSheet } from 'react-native'
+import { FlatList, StyleSheet, TouchableOpacity, View } from 'react-native'
 
+import { Input } from '../components/common'
 import Message from '../components/message'
 import colors from '../config/colors'
 import mock from '../mock'
@@ -15,14 +17,24 @@ const Chat = ({ navigation }) => {
   const renderItem = ({ item }) => <Message message={item} />
 
   return (
-    <FlatList
-      data={mock}
-      ref={flatListRef}
-      renderItem={renderItem}
-      contentContainerStyle={styles.view}
-      onContentSizeChange={handleScrollToEnd}
-      keyExtractor={(message) => message.id}
-    />
+    <>
+      <FlatList
+        data={mock}
+        ref={flatListRef}
+        renderItem={renderItem}
+        contentContainerStyle={styles.view}
+        showsVerticalScrollIndicator={false}
+        onContentSizeChange={handleScrollToEnd}
+        keyExtractor={(message) => message.id}
+        ListFooterComponent={<View style={{ paddingVertical: 4 }} />}
+      />
+      <View style={styles.inputContainer}>
+        <Input placeholder="Tapez un message" size={34} width="87%" />
+        <TouchableOpacity>
+          <MaterialCommunityIcons name="send" style={styles.icon} />
+        </TouchableOpacity>
+      </View>
+    </>
   )
 }
 
@@ -35,5 +47,18 @@ const styles = StyleSheet.create({
   view: {
     paddingHorizontal: 15,
     backgroundColor: colors.light
+  },
+  icon: {
+    fontSize: 28,
+    color: colors.dark
+  },
+  inputContainer: {
+    paddingHorizontal: 15,
+    backgroundColor: colors.light,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    borderTopColor: 'transparent',
+    borderTopWidth: 1
   }
 })
