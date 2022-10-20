c6887283205bb7500e66f2f7dfa682443fbd8572
Ks Krimi <ny.kalash@gmail.com>
2022-10-20 10:58:31 +0200

    refactor: change le text du composant button en lowercase

--- src/components/common/button/index.js
+++ src/components/common/button/index.js
@@ -27,7 +27,7 @@ const styles = StyleSheet.create({
   text: {
     color: colors.white,
     fontSize: 18,
-    textTransform: 'uppercase',
+    // textTransform: 'uppercase',
     fontWeight: 'bold'
   }
 })
