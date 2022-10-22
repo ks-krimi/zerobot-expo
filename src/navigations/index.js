3483ebfedf7d2d9b7d46021833683a6bd14371bd
Ks Krimi <ny.kalash@gmail.com>
2022-10-22 09:13:03 +0200

    feat: export le ref et la function navigation pour le composant NavigationContainer

--- src/navigations/index.js
+++ src/navigations/index.js
@@ -1,4 +1,5 @@
 import AppNavigator from './AppNavigator'
+import { navigate, navigationRef } from './navigationRef'
 import navigationTheme from './navigationTheme'
 
-export { AppNavigator, navigationTheme }
+export { AppNavigator, navigate, navigationRef, navigationTheme }
