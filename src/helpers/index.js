4ac086a66aacf515f246c713c69b582456213256
Ks Krimi <ny.kalash@gmail.com>
2022-10-22 06:18:04 +0200

    feat: crée un helper pour valider la formulaire d'inscription

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -19,3 +19,29 @@ export const validateLogin = (values) => {
 
   return errors
 }
+
+export const validateRegister = (values) => {
+  const errors = {}
+
+  if (!values.username) {
+    errors.username = 'Pseudo est vide'
+  } else if (values.username.trim() === '') {
+    errors.password = "Espace n'est pas authorisé"
+  }
+
+  if (!values.password) {
+    errors.password = 'Mot de passe est vide'
+  } else if (values.password.trim() === '') {
+    errors.password = "Espace n'est pas authorisé"
+  } else if (values.password.length < 4) {
+    errors.password = 'Au moin 04 caractères'
+  }
+
+  if (!values.email) {
+    errors.email = 'Email est vide'
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.email = 'Email invalide'
+  }
+
+  return errors
+}
