9d6ebdf00b780ec75660bdb2e5f72d40bbb4d325
Ks Krimi <ny.kalash@gmail.com>
2022-10-22 06:17:14 +0200

    feat: valider la formulaire du login en utilisant formik

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,21 +1,49 @@
+import { Formik } from 'formik'
 import React from 'react'
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 
 import colors from '../../config/colors'
+import { validateLogin } from '../../helpers'
 import { Button, Input } from '../common'
 
 const Login = ({ setLogin, setLoggedIn }) => {
   return (
     <View style={styles.container}>
-      <Input icon="email" placeholder="Enter votre email" />
-      <Input icon="key-chain-variant" placeholder="Enter votre mot de passe" />
-      <Button
-        onPress={() => {
+      <Formik
+        initialValues={{ username: '', password: '' }}
+        validate={validateLogin}
+        onSubmit={(values, helpers) => {
+          console.log(values)
           setLoggedIn(true)
+          helpers.resetForm()
         }}
-        title="Se connecter"
-        style={{ padding: 8 }}
-      />
+      >
+        {({ handleChange, handleBlur, handleSubmit, isValid, values }) => (
+          <View>
+            <Input
+              icon="email"
+              placeholder="Enter votre email"
+              onChangeText={handleChange('username')}
+              onBlur={handleBlur('username')}
+              value={values.username}
+            />
+            <Input
+              secureTextEntry={true}
+              icon="key-chain-variant"
+              placeholder="Enter votre mot de passe"
+              onChangeText={handleChange('password')}
+              onBlur={handleBlur('password')}
+              value={values.password}
+            />
+            <Button
+              disabled={!isValid}
+              onPress={handleSubmit}
+              title="Se connecter"
+              style={{ padding: 8 }}
+            />
+          </View>
+        )}
+      </Formik>
 
       <TouchableOpacity onPress={() => setLogin(false)}>
         <Text style={{ color: colors.dark }}>
