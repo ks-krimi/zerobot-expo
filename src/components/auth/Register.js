bdc29709928240d5bc21dedc8977ac2170718702
Ks Krimi <ny.kalash@gmail.com>
2022-10-22 06:18:59 +0200

    feat: valider la formulaire d'inscription en utilisant formik

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -1,17 +1,55 @@
+import { Formik } from 'formik'
 import React from 'react'
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 
 import colors from '../../config/colors'
+import { validateRegister } from '../../helpers'
 import { Button, Input } from '../common'
 
 const Register = ({ setLogin }) => {
   return (
     <View style={styles.container}>
-      <Input icon="account" placeholder="Enter votre pseudo" />
-      <Input icon="email" placeholder="Enter votre email" />
-      <Input icon="key-chain-variant" placeholder="Enter votre mot de passe" />
-      <Button onPress={() => {}} title="S'inscrire" style={{ padding: 8 }} />
-
+      <Formik
+        initialValues={{ username: '', email: '', password: '' }}
+        validate={validateRegister}
+        onSubmit={(values, helpers) => {
+          console.log(values)
+          helpers.resetForm()
+        }}
+      >
+        {({ handleChange, handleBlur, handleSubmit, isValid, values }) => (
+          <View>
+            <Input
+              icon="account"
+              placeholder="Enter votre pseudo"
+              onChangeText={handleChange('username')}
+              onBlur={handleBlur('username')}
+              value={values.username}
+            />
+            <Input
+              icon="email"
+              placeholder="Enter votre email"
+              onChangeText={handleChange('email')}
+              onBlur={handleBlur('email')}
+              value={values.email}
+            />
+            <Input
+              secureTextEntry={true}
+              icon="key-chain-variant"
+              placeholder="Enter votre mot de passe"
+              onChangeText={handleChange('password')}
+              onBlur={handleBlur('password')}
+              value={values.password}
+            />
+            <Button
+              disabled={!isValid}
+              onPress={handleSubmit}
+              title="S'inscrire"
+              style={{ padding: 8 }}
+            />
+          </View>
+        )}
+      </Formik>
       <TouchableOpacity onPress={() => setLogin(true)}>
         <Text style={{ color: colors.dark }}>
           J'ai déja un compte, se connecter.
