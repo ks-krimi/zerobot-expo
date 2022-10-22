7857451c533c41ce5e8afaff7d22c0b5e85c143e
Ks Krimi <ny.kalash@gmail.com>
2022-10-22 21:11:46 +0200

    conf(redux): implemente le redux provider

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 import { NavigationContainer } from '@react-navigation/native'
 import { StatusBar } from 'expo-status-bar'
+import { Provider } from 'react-redux'
 
 import { AppNavigator, navigationRef, navigationTheme } from './src/navigations'
+import store from './src/store'
 
 export default function App() {
   return (
-    <NavigationContainer ref={navigationRef} theme={navigationTheme}>
-      <StatusBar style="auto" />
-      <AppNavigator />
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer ref={navigationRef} theme={navigationTheme}>
+        <StatusBar style="auto" />
+        <AppNavigator />
+      </NavigationContainer>
+    </Provider>
   )
 }
