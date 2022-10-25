3d57b9c1cc9bb50ec700014dc6aaa7df18675d6e
Ks Krimi <ny.kalash@gmail.com>
2022-10-25 14:36:38 +0200

    feat: export le composant Modal

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -2,5 +2,6 @@ import Button from './button'
 import Icon from './icon'
 import Input from './input'
 import Text from './text'
+import Modal from './modal'
 
-export { Button, Icon, Input, Text }
+export { Button, Icon, Input, Modal, Text }
